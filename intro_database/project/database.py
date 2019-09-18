e3565957b80c41c7ae41b71734c76374d48723a9
Matin Sasanpour <matin.sasan@gmail.com>
2019-09-18 13:43:05 +0430

    not ideal class solution

--- intro_database/project/database.py
+++ intro_database/project/database.py
@@ -4,14 +4,24 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-connection_pool = pool.SimpleConnectionPool(
-    1,
-    3,
-    user=os.getenv('USER'),
-    password=os.getenv('PASS'),
-    database=os.getenv('DATABASE'),
-    host="localhost"
-)
+
+# NOT IDEAL
+class ConnectionPool:
+    def __init__(self):
+        self.connection_pool = pool.SimpleConnectionPool(
+            1,
+            3,
+            user=os.getenv('USER'),
+            password=os.getenv('PASS'),
+            database=os.getenv('DATABASE'),
+            host="localhost"
+        )
+
+    def __enter__(self):
+        return self.connection_pool.getconn()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
 # def connect():

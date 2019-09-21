950f01be575c2cf80282424474357a8361808ff4
Matin Sasanpour <matin.sasan@gmail.com>
2019-09-22 00:12:17 +0330

    more lambdas

--- OOP/package1/lambdas.py
+++ OOP/package1/lambdas.py
@@ -9,9 +9,23 @@ def lambda1(x):
 
 def alter(values, check):
     return [val for val in values if check(val)]
+    # return list(filter(check, values))
+
+
+def remove_numbers(value):
+    return alter(value, lambda x: x not in [str(n) for n in range(10)])
+
+
+def skip_letter(value, letter):
+    return alter(value, lambda x: x != letter)
 
 
 my_list = [1, 2, 3, 4, 5]
 another_list = alter(my_list, lambda x: x != 5)
 
 print(another_list)
+
+print(remove_numbers('progr3ammer'))
+
+
+print(skip_letter("coding", 'i'))
